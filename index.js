82703170a4f79a4ddf0b191a18b700ff1a7f92e5
Jason Back <jason.back01@gmail.com>
2023-09-28 20:00:07 -0400

    implemented random cocktail

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
     function searchByName() {
-        fetch(
-            "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-        )
+        const inputForm = document.querySelector(".search");
+        inputForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const input = document.querySelector("input");
+            const searchType = document.querySelectorAll("option");
+            console.log(searchType.value);
+            fetch(
+                `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    const content = document.querySelector(".content");
+                    data.drinks.forEach((drink) => {
+                        console.log(drink);
+                        const list = document.createElement("ul");
+                        const item = document.createElement("div");
+                        content.appendChild(list);
+                        item.className = "item";
+                        item.innerText =
+                            drink.strDrink + ": " + drink.strInstructions;
+                        list.appendChild(item);
+                    });
+                });
+        });
+    }
+    searchByName();
+    const random = document.querySelector(".random");
+    random.addEventListener("click", () => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
             .then((res) => res.json())
             .then((data) => {
-                const list = document.createElement("ul");
                 const content = document.querySelector(".content");
-                content.appendChild(list);
                 data.drinks.forEach((drink) => {
                     console.log(drink);
-                    const item = document.createElement("li");
-                    item.innerText = drink.strDrink;
+                    const list = document.createElement("ul");
+                    const item = document.createElement("div");
+                    content.appendChild(list);
+                    item.className = "item";
+                    item.innerText =
+                        drink.strDrink + ": " + drink.strInstructions;
                     list.appendChild(item);
                 });
             });
-    }
-    searchByName();
+    });
 });
