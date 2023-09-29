b88cc6e690427c5d15bdc961ed11868da612a455
Jason Back <jason.back01@gmail.com>
2023-09-28 20:31:18 -0400

    added images to cocktails

--- index.js
+++ index.js
@@ -4,25 +4,50 @@ document.addEventListener("DOMContentLoaded", () => {
         inputForm.addEventListener("submit", (event) => {
             event.preventDefault();
             const input = document.querySelector("input");
-            const searchType = document.querySelectorAll("option");
-            console.log(searchType.value);
-            fetch(
-                `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
-            )
-                .then((res) => res.json())
-                .then((data) => {
-                    const content = document.querySelector(".content");
-                    data.drinks.forEach((drink) => {
-                        console.log(drink);
-                        const list = document.createElement("ul");
-                        const item = document.createElement("div");
-                        content.appendChild(list);
-                        item.className = "item";
-                        item.innerText =
-                            drink.strDrink + ": " + drink.strInstructions;
-                        list.appendChild(item);
+            const searchType = document.querySelector(".search-items");
+            const option = searchType.value;
+            if (option === "name") {
+                fetch(
+                    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
+                )
+                    .then((res) => res.json())
+                    .then((data) => {
+                        const content = document.querySelector(".content");
+                        data.drinks.forEach((drink) => {
+                            console.log(drink);
+                            const image = document.createElement("img");
+                            image.src = `${drink.strDrinkThumb}`;
+                            image.classList.add(".images");
+                            content.append(image);
+                            const list = document.createElement("ul");
+                            const item = document.createElement("div");
+                            content.appendChild(list);
+                            item.className = "item";
+                            item.innerText =
+                                drink.strDrink + ": " + drink.strInstructions;
+                            list.appendChild(item);
+                        });
                     });
-                });
+            } else {
+                fetch(
+                    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input.value}`
+                )
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                        const content = document.querySelector(".content");
+                        data.drinks.forEach((drink) => {
+                            console.log(drink);
+                            const list = document.createElement("ul");
+                            const item = document.createElement("div");
+                            content.appendChild(list);
+                            item.className = "item";
+                            item.innerText =
+                                drink.strDrink + ": " + drink.strInstructions;
+                            list.appendChild(item);
+                        });
+                    });
+            }
         });
     }
     searchByName();
