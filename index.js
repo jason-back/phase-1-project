833099a94ce3ad5ef9bdbb6c34898dec2e41d721
Jason Back <jason.back01@gmail.com>
2023-10-01 19:17:17 -0400

    added getDrinkInfo function to use for all search options

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function searchByName() {
-        const inputForm = document.querySelector(".search");
-        inputForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const input = document.querySelector("input");
-            const searchType = document.querySelector(".search-items");
-            const option = searchType.value;
-            if (option === "name") {
-                fetch(
-                    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
-                )
-                    .then((res) => res.json())
-                    .then((data) => {
-                        const content = document.querySelector(".content");
-                        data.drinks.forEach((drink) => {
-                            console.log(drink);
-                            const image = document.createElement("img");
-                            image.src = `${drink.strDrinkThumb}`;
-                            image.classList.add(".images");
-                            content.append(image);
-                            const list = document.createElement("ul");
-                            const item = document.createElement("div");
-                            content.appendChild(list);
-                            item.className = "item";
-                            item.innerText =
-                                drink.strDrink + ": " + drink.strInstructions;
-                            list.appendChild(item);
-                        });
-                    });
-            } else {
-                fetch(
-                    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input.value}`
-                )
-                    .then((res) => res.json())
-                    .then((data) => {
-                        console.log(data);
-                        const content = document.querySelector(".content");
-                        data.drinks.forEach((drink) => {
-                            console.log(drink);
-                            const list = document.createElement("ul");
-                            const item = document.createElement("div");
-                            content.appendChild(list);
-                            item.className = "item";
-                            item.innerText =
-                                drink.strDrink + ": " + drink.strInstructions;
-                            list.appendChild(item);
-                        });
-                    });
-            }
+    function clearSearchResults(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    function getDrinkInfo(data) {
+        const content = document.querySelector(".content");
+        clearSearchResults(content);
+        data.drinks.forEach((drink) => {
+            console.log(drink);
+            const image = document.createElement("img");
+            image.src = `${drink.strDrinkThumb}`;
+            image.classList.add(".images");
+            content.append(image);
+            const list = document.createElement("ul");
+            const item = document.createElement("div");
+            content.appendChild(list);
+            item.className = "item";
+            item.innerText = drink.strDrink + ": " + drink.strInstructions;
+            list.appendChild(item);
         });
     }
-    searchByName();
+    const inputForm = document.querySelector(".search");
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input");
+        const searchType = document.querySelector(".search-items");
+        const option = searchType.value;
+
+        // name option
+
+        if (option === "name") {
+            fetch(
+                `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    getDrinkInfo(data);
+                });
+        }
+
+        // alcohol option
+        else {
+            fetch(
+                `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${input.value}`
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    data.drinks.forEach((drink) => {
+                        fetch(
+                            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink.strDrink}`
+                        )
+                            .then((res) => res.json())
+                            .then((data) => {
+                                getDrinkInfo(data);
+                            });
+                    });
+                });
+        }
+    });
+
+    // random option
     const random = document.querySelector(".random");
     random.addEventListener("click", () => {
         fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
             .then((res) => res.json())
             .then((data) => {
-                const content = document.querySelector(".content");
-                data.drinks.forEach((drink) => {
-                    console.log(drink);
-                    const list = document.createElement("ul");
-                    const item = document.createElement("div");
-                    content.appendChild(list);
-                    item.className = "item";
-                    item.innerText =
-                        drink.strDrink + ": " + drink.strInstructions;
-                    list.appendChild(item);
-                });
+                getDrinkInfo(data);
             });
     });
 });
